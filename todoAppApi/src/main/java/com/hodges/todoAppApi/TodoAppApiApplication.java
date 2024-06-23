e5dcf80997de5a329345d63b2fd2814c7b8ac666
koolkat254 <75220971+koolkat254@users.noreply.github.com>
2024-06-23 16:47:00 -0500

    Api init

---
+++ todoAppApi/src/main/java/com/hodges/todoAppApi/TodoAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.hodges.todoAppApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppApiApplication.class, args);
+	}
+
+}
